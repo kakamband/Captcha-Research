b0bd6dc56e07293224a594e673162acf078396e5
Israel Alagbe <israelalagbe53@gmail.com>
2020-02-13 14:25:31 +0100

    Moved db config of save.php

--- save.php
+++ save.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "captcha";
+require_once "./config.php";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
